047d346d36fb6e6492060335546b60abbc08998b
Daniel Mangowi <mangowidaniel@gmail.com>
2017-06-14 01:47:35 +0200

    messages contoroller

---
+++ TasksManagementMVCApp/Controllers/MessagesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TasksManagementMVCApp.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET: Messages
+        public ActionResult VieAll()
+        {
+            return View();
+        }
+
+        public ActionResult Reply()
+        {
+            return View();
+        }
+    }
+}
