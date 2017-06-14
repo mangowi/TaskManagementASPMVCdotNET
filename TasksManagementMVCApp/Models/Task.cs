96c3cc3128263d97f7376a18cefd7dc950fbbe13
Daniel Mangowi <mangowidaniel@gmail.com>
2017-06-14 17:55:56 +0200

    razor validation done

--- TasksManagementMVCApp/Models/Task.cs
+++ TasksManagementMVCApp/Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,14 +11,22 @@ namespace TasksManagementMVCApp.Models
     public class Task
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public DateTime? DueDate { get; set; }
+        [Required]
         public int AssignedToId { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
         public int AssociatedMessageId { get; set; }
         public DateTime? Created { get; set; }
+        [StringLength(2000, MinimumLength =20)]
         public string Notes { get; set; }
+        [Required]
         public bool Completed { get; set; }
 
         //Navigation Properties
